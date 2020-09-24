dbc499fcb8c846dd2f6ed745d05cb94bd0da6733
Kārlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2020-09-24 14:33:58 +0300

    Add AliceMain file with logic

---
+++ ex03-alice/AliceMain.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+#include <map>
+#include "Node.h"
+
+using namespace ds_course;
+using namespace std;
+
+int findNodeHeadNumber(vector<int> first, vector<int> second);
+Node addChildren(int nodeNumber, map<int, vector<int>> nodeMap);
+
+int main()
+{
+
+    std::string inputLine;
+    std::map<int, Node> nodeList;
+    std::map<int, std::vector<int>> inputData;
+
+    std::vector<int> mainNumberArray;
+    std::vector<int> secondaryNumberArray;
+    int headNodeNumber;
+
+    while (getline(cin, inputLine))
+    {
+        istringstream sstr(inputLine);
+        if (sstr.peek() == '0')
+        {
+            break;
+        }
+        else
+        {
+            int number;
+            int parentNodeNumber;
+            vector<int> childNodeNumbers;
+            bool isMain = true;
+
+            while (sstr >> number)
+            {
+                if (isMain)
+                {
+                    parentNodeNumber = number;
+                    mainNumberArray.push_back(number);
+
+                    isMain = false;
+                }
+                else
+                {
+                    childNodeNumbers.push_back(number);
+                    secondaryNumberArray.push_back(number);
+                }
+            }
+
+            inputData.insert(std::pair<int, vector<int>>(parentNodeNumber, childNodeNumbers));
+        }
+    }
+
+    headNodeNumber = findNodeHeadNumber(mainNumberArray, secondaryNumberArray);
+
+    Node headNode = addChildren(headNodeNumber, inputData);
+
+    headNode.printPreorder();
+    cout << "0\n";
+
+    /** TODO:
+     * We have found the head node number, we can start building tree
+     * But now I am thinking that when we read we don't need to make nodes
+     * We only store the values in a data structure and after we have found
+     * the head number we start to create tree by looking at the created structure
+     */
+
+    return 0;
+}
+
+Node addChildren(int nodeNumber, map<int, vector<int>> nodeMap)
+{
+    Node node(nodeNumber);
+
+    map<int, vector<int>>::iterator it = nodeMap.find(nodeNumber);
+
+    if (it == nodeMap.end())
+    {
+        return node;
+    }
+
+    for (int i = 0; i < nodeMap.at(nodeNumber).size(); i++)
+    {
+        Node child = addChildren(nodeMap.at(nodeNumber)[i], nodeMap);
+        node.addChild(child);
+    }
+
+    return node;
+}
+
+int findNodeHeadNumber(vector<int> first, vector<int> second)
+{
+    int res = -1;
+
+    for (int i = 0; i < first.size(); i++)
+    {
+        bool dublicateFound = false;
+
+        for (int j = 0; j < second.size(); j++)
+        {
+            if (first[i] == second[j])
+            {
+                dublicateFound = true;
+                break;
+            }
+        }
+
+        if (!dublicateFound)
+        {
+            return first[i];
+        }
+    }
+
+    return res;
+}
